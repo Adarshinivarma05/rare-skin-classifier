b295dbb68ddaab5673cfaa2b46b4f63b05b0ba15
Adarshinivarma05 <adarshinip2k25@gmail.com>
2025-05-18 12:45:53 +0000

    Update utils/visualizer.py from Colab

--- utils/visualizer.py
+++ utils/visualizer.py
@@ -1,1 +1,11 @@
-# Visualization utilities (GradCAM, prototype patches)
+import matplotlib.pyplot as plt
+
+def plot_loss_curve(losses):
+    plt.plot(losses)
+    plt.title("Training Loss Curve")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.grid()
+    plt.show()
+
+# Your actual code here
