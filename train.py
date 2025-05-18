d002475f0aa03c2daddf8101e64f3f70c373ddcc
Adarshinivarma05 <adarshinip2k25@gmail.com>
2025-05-18 12:39:19 +0000

    Update train.py from Colab

--- train.py
+++ train.py
@@ -1,22 +1,48 @@
-from medmnist import DermaMNIST
-from medmnist import INFO
+import torch
+from torch import nn, optim
+from models.protopnet_skin_classifier import ProtoPNet
+from medmnist import INFO, DermaMNIST
 from torchvision import transforms
 from torch.utils.data import DataLoader
+from utils.visualizer import plot_loss_curve
 
 info = INFO['dermamnist']
 DataClass = DermaMNIST
 
 data_transform = transforms.Compose([
-transforms.ToTensor(),
-transforms.Resize((224, 224)), # for ResNet or ProtoPNet
-transforms.Normalize(mean=[.5], std=[.5])
+    transforms.ToTensor(),
+    transforms.Resize((224, 224)),
+    transforms.Normalize(mean=[.5], std=[.5])
 ])
-train_dataset = DataClass(split='train', transform=data_transform, download=True)
-val_dataset = DataClass(split='val', transform=data_transform, download=True)
-test_dataset = DataClass(split='test', transform=data_transform, download=True)
 
+train_dataset = DataClass(split='train', transform=data_transform, download=True)
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
-test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = ProtoPNet(num_prototypes=30, num_classes=7).to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=1e-4)
+
+EPOCHS = 10
+losses = []
+
+for epoch in range(EPOCHS):
+    model.train()
+    total_loss = 0
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device).squeeze()  # labels shape fix
+        optimizer.zero_grad()
+        outputs, _ = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+    losses.append(total_loss)
+    print(f"Epoch {epoch+1} Loss: {total_loss:.4f}")
+
+torch.save(model.state_dict(), 'protopnet_skin.pt')
+plot_loss_curve(losses)
+
 
 # Your actual code here
