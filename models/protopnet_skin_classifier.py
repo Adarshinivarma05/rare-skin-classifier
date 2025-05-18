2577a1ef29c12c8f53912fe9a9e171eac142602c
Adarshinivarma05 <adarshinip2k25@gmail.com>
2025-05-18 12:32:55 +0000

    Update models/protopnet_skin_classifier.py from Colab

--- models/protopnet_skin_classifier.py
+++ models/protopnet_skin_classifier.py
@@ -1,1 +1,19 @@
-hi
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class ProtoPNet(nn.Module):
+def init(self, num_prototypes, num_classes):
+super().init()
+self.backbone = models.resnet18(pretrained=True)
+self.backbone.fc = nn.Identity() # Remove last FC
+self.prototype_vectors = nn.Parameter(torch.rand(num_prototypes, 512))
+self.linear = nn.Linear(num_prototypes, num_classes, bias=False)
+
+def forward(self, x):
+    features = self.backbone(x)  # shape: (B, 512)
+    dists = torch.cdist(features, self.prototype_vectors)  # shape: (B, P)
+    sims = -dists  # convert distance to similarity
+    logits = self.linear(sims)
+    return logits, sims
+
