9f80e3595c77899f5c673bb3c1511fac92e55e26
Adarshinivarma05 <adarshinip2k25@gmail.com>
2025-05-18 12:22:08 +0000

    Update train.py from Colab

--- train.py
+++ train.py
@@ -1,3 +1,22 @@
-print("Training script running...")
+from medmnist import DermaMNIST
+from medmnist import INFO
+from torchvision import transforms
+from torch.utils.data import DataLoader
+
+info = INFO['dermamnist']
+DataClass = DermaMNIST
+
+data_transform = transforms.Compose([
+transforms.ToTensor(),
+transforms.Resize((224, 224)), # for ResNet or ProtoPNet
+transforms.Normalize(mean=[.5], std=[.5])
+])
+train_dataset = DataClass(split='train', transform=data_transform, download=True)
+val_dataset = DataClass(split='val', transform=data_transform, download=True)
+test_dataset = DataClass(split='test', transform=data_transform, download=True)
+
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
 # Your actual code here
