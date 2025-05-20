2abb201540d4d969d11f024540410903c94f26cd
h-rsh-19 <152505473+h-rsh-19@users.noreply.github.com>
2025-05-20 19:04:14 +0530

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -33,3 +33,38 @@ def get_dataloaders(batch_size=64, val_split=0.2):
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
 
     return train_loader, val_loader
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, random_split
+
+def get_dataloaders(batch_size=64, val_split=0.2):
+    # Data augmentations for train set
+    train_transforms = transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(15),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],  # ImageNet normalization
+                             std=[0.229, 0.224, 0.225]),
+    ])
+
+    val_transforms = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225]),
+    ])
+
+    full_dataset = datasets.ImageFolder('data/train', transform=train_transforms)
+    val_size = int(len(full_dataset) * val_split)
+    train_size = len(full_dataset) - val_size
+
+    train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
+    val_dataset.dataset.transform = val_transforms  # change val transforms
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
+
+    return train_loader, val_loader
