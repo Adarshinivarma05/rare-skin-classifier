87ce19f6545c07fcf4e448b549a1e70be36dd0ed
h-rsh-19 <152505473+h-rsh-19@users.noreply.github.com>
2025-05-20 20:59:42 +0530

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -3,21 +3,21 @@ from torchvision import transforms
 from torch.utils.data import DataLoader, random_split
 import torch
 
-def get_dataloaders(batch_size=64, val_split=0.1, seed=42, shuffle=True, image_size=224):
+def get_data_loaders(batch_size=64, val_split=0.1, seed=42, shuffle=True, image_size=224):
     info = INFO['dermamnist']
     DataClass = DermaMNIST
 
     transform = transforms.Compose([
-        transforms.ToTensor(),
         transforms.Resize((image_size, image_size)),
-        transforms.Normalize(mean=[0.5], std=[0.5])  # DermaMNIST is grayscale
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[.5], std=[.5])  # Grayscale
     ])
 
-    # Load datasets
+    # Load full train and test splits from medmnist
     full_train_dataset = DataClass(split='train', transform=transform, download=True)
     test_dataset = DataClass(split='test', transform=transform, download=True)
 
-    # Train/Validation split
+    # Create validation split from training set
     total_train = len(full_train_dataset)
     val_size = int(val_split * total_train)
     train_size = total_train - val_size
@@ -25,7 +25,7 @@ def get_dataloaders(batch_size=64, val_split=0.1, seed=42, shuffle=True, image_s
     generator = torch.Generator().manual_seed(seed)
     train_dataset, val_dataset = random_split(full_train_dataset, [train_size, val_size], generator=generator)
 
-    # DataLoaders
+    # Return DataLoaders
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
