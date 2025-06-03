cb76456875744466579323accaf035af479901f4
Adarshinivarma05 <adarshinip2k25@gmail.com>
2025-06-03 15:12:39 +0000

    Update test.py from Colab

--- test.py
+++ test.py
@@ -1,29 +1,16 @@
+# test.py
 import torch
 from models.protopnet_skin_classifier import ProtoPNet
 from utils.data_loader import get_dataloaders
-import torch.nn as nn
+from utils.train_utils import calculate_metrics
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = ProtoPNet().to(device)
-model.load_state_dict(torch.load("protopnet_best.pth"))
+model.load_state_dict(torch.load("best_model.pth"))
 model.eval()
 
-_, test_loader = get_dataloaders()
+_, _, test_loader = get_dataloaders()
 
-correct, total, test_loss = 0, 0, 0.0
-criterion = nn.CrossEntropyLoss()
+acc, f1 = calculate_metrics(model, test_loader, device)
+print(f"Test Accuracy: {acc:.2f}% | F1 Score: {f1:.4f}")
 
-with torch.no_grad():
-   for images, labels in test_loader:
-       images, labels = images.to(device), labels.squeeze().long().to(device)
-       outputs = model(images)
-       loss = criterion(outputs, labels)
-       test_loss += loss.item()
-       _, preds = torch.max(outputs, 1)
-       correct += (preds == labels).sum().item()
-       total += labels.size(0)
-
-avg_loss = test_loss / len(test_loader)
-accuracy = 100 * correct / total
-
-print(f"Test Loss: {avg_loss:.4f} | Accuracy: {accuracy:.2f}%")
