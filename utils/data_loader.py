68e9ba9a7b237e883d603375c691fbdea1974fd6
Adarshinivarma05 <adarshinip2k25@gmail.com>
2025-06-03 15:08:50 +0000

    Update utils/data_loader.py from Colab

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,31 +1,32 @@
+# utils/data_loader.py
 from medmnist import INFO, DermaMNIST
 from torchvision import transforms
 from torch.utils.data import DataLoader
-import torch
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Subset
 
-def get_dataloaders(batch_size=64):
+def get_dataloaders(batch_size=64, val_split=0.2):
    info = INFO['dermamnist']
    DataClass = DermaMNIST
 
-   train_transform = transforms.Compose([
-       transforms.Resize((224, 224)),
-       transforms.RandomHorizontalFlip(),
-       transforms.RandomRotation(20),
-       transforms.ToTensor(),
-       transforms.Normalize(mean=[.5], std=[.5])
-   ])
-
-   test_transform = transforms.Compose([
+   transform = transforms.Compose([
        transforms.Resize((224, 224)),
        transforms.ToTensor(),
        transforms.Normalize(mean=[.5], std=[.5])
    ])
 
-   train_dataset = DataClass(split='train', transform=train_transform, download=True)
-   test_dataset = DataClass(split='test', transform=test_transform, download=True)
+   full_dataset = DataClass(split='train', transform=transform, download=True)
+   test_dataset = DataClass(split='test', transform=transform, download=True)
 
-   train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+   indices = list(range(len(full_dataset)))
+   train_idx, val_idx = train_test_split(indices, test_size=val_split, stratify=full_dataset.labels)
+
+   train_set = Subset(full_dataset, train_idx)
+   val_set = Subset(full_dataset, val_idx)
+
+   train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+   val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False)
    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-   return train_loader, test_loader
+   return train_loader, val_loader, test_loader
 
