fe179664a5f790a193ee09edfe0ebd841dd23a07
Adarshinivarma05 <adarshinip2k25@gmail.com>
2025-06-03 14:12:16 +0000

    Update utils/data_loader.py from Colab

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,30 +1,31 @@
 from medmnist import INFO, DermaMNIST
 from torchvision import transforms
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader
 import torch
 
-def get_dataloaders(batch_size=64, val_split=0.1, seed=42, shuffle=True, image_size=224):
-    info = INFO['dermamnist']
-    DataClass = DermaMNIST
+def get_dataloaders(batch_size=64):
+   info = INFO['dermamnist']
+   DataClass = DermaMNIST
 
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Resize((image_size, image_size)),
-        transforms.Normalize(mean=[0.5]*3, std=[0.5]*3)
-    ])
+   train_transform = transforms.Compose([
+       transforms.Resize((224, 224)),
+       transforms.RandomHorizontalFlip(),
+       transforms.RandomRotation(20),
+       transforms.ToTensor(),
+       transforms.Normalize(mean=[.5], std=[.5])
+   ])
 
-    full_train_dataset = DataClass(split='train', transform=transform, download=True)
-    test_dataset = DataClass(split='test', transform=transform, download=True)
+   test_transform = transforms.Compose([
+       transforms.Resize((224, 224)),
+       transforms.ToTensor(),
+       transforms.Normalize(mean=[.5], std=[.5])
+   ])
 
-    total_train = len(full_train_dataset)
-    val_size = int(val_split * total_train)
-    train_size = total_train - val_size
+   train_dataset = DataClass(split='train', transform=train_transform, download=True)
+   test_dataset = DataClass(split='test', transform=test_transform, download=True)
 
-    generator = torch.Generator().manual_seed(seed)
-    train_dataset, val_dataset = random_split(full_train_dataset, [train_size, val_size], generator=generator)
+   train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+   test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+   return train_loader, test_loader
 
-    return train_loader, val_loader, test_loader
