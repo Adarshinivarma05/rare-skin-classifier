79c1b9e21eb2727a6ea804e4544c9964570f8800
Adarshinivarma05 <adarshinip2k25@gmail.com>
2025-06-15 08:25:36 +0000

    Update utils/train_utils.py from Colab

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -1,35 +1,25 @@
 import torch
-from sklearn.metrics import accuracy_score, f1_score
-
+from sklearn.metrics import f1_score, accuracy_score
 
 def epoch_step(model, loader, criterion, optimizer=None, scaler=None, device='cpu'):
-    """
-    Run one epoch. If `optimizer` is None, runs in eval mode.
-    Supports AMP mixed precision through `scaler`.
-    Returns loss, accuracy, f1.
-    """
     is_train = optimizer is not None
     model.train() if is_train else model.eval()
 
-    running_loss, all_preds, all_labels = 0.0, [], []
+    running_loss = 0
+    all_preds, all_labels = [], []
 
     for imgs, labels in loader:
-        imgs   = imgs.to(device, non_blocking=True)
-        labels = labels.squeeze().long().to(device, non_blocking=True)
+        imgs, labels = imgs.to(device), labels.squeeze().long().to(device)
 
-        with torch.cuda.amp.autocast(enabled=scaler is not None):
+        with torch.amp.autocast(device_type='cuda', enabled=scaler is not None):
             outputs = model(imgs)
             loss = criterion(outputs, labels)
 
         if is_train:
-            optimizer.zero_grad(set_to_none=True)
-            if scaler:
-                scaler.scale(loss).backward()
-                scaler.step(optimizer)
-                scaler.update()
-            else:
-                loss.backward()
-                optimizer.step()
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+            optimizer.zero_grad()
 
         running_loss += loss.item()
         preds = outputs.argmax(1)
@@ -37,6 +27,12 @@ def epoch_step(model, loader, criterion, optimizer=None, scaler=None, device='cp
         all_labels.extend(labels.cpu().numpy())
 
     loss_avg = running_loss / len(loader)
-    acc      = accuracy_score(all_labels, all_preds)
-    f1       = f1_score(all_labels, all_preds, average='weighted')
+    acc = accuracy_score(all_labels, all_preds)
+    f1 = f1_score(all_labels, all_preds, average='weighted')
     return loss_avg, acc, f1
+
+
+
+
+
+
