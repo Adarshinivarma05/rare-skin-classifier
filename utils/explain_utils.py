c62a463de85384128099b35fe6f95ccee9467c26
h-rsh-19 <152505473+h-rsh-19@users.noreply.github.com>
2025-06-15 20:01:04 +0530

    Update explain_utils.py

--- utils/explain_utils.py
+++ utils/explain_utils.py
@@ -1,15 +1,15 @@
 import torch
 from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 import numpy as np
 
-def compute_gradcam(model, input_tensor, target_layer):
-    cam = GradCAM(model=model, target_layers=[target_layer], use_cuda=input_tensor.is_cuda)
-    grayscale_cam = cam(input_tensor=input_tensor)[0]
-    img = input_tensor.squeeze().permute(1, 2, 0).cpu().numpy()
+
+def gradcam_vis(model, img_tensor, target_layer, device):
+    model.eval()
+    target_class = torch.argmax(model(img_tensor.to(device))).item()
+    cam = GradCAM(model, [target_layer], use_cuda=device.type == "cuda")
+    grayscale_cam = cam(img_tensor.to(device), [ClassifierOutputTarget(target_class)])[0]
+    img = img_tensor.squeeze().permute(1, 2, 0).cpu().numpy()
     img = (img - img.min()) / (img.max() - img.min())
-    vis = show_cam_on_image(img, grayscale_cam, use_rgb=True)
-    return vis
-
-
-
+    return show_cam_on_image(img, grayscale_cam, use_rgb=True)
