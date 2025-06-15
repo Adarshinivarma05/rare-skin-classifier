70d9566565329f0a42dd3efb6e4a03d58982523d
Adarshinivarma05 <adarshinip2k25@gmail.com>
2025-06-15 07:59:14 +0000

    Update utils/train_utils.py from Colab

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -1,24 +1,42 @@
-# utils/train_utils.py
 import torch
-from sklearn.metrics import f1_score
+from sklearn.metrics import accuracy_score, f1_score
 
-def calculate_metrics(model, dataloader, device):
-   model.eval()
-   correct = 0
-   total = 0
-   all_preds, all_labels = [], []
 
-   with torch.no_grad():
-       for images, labels in dataloader:
-           images, labels = images.to(device), labels.squeeze().long().to(device)
-           outputs = model(images)
-           _, preds = torch.max(outputs, 1)
-           correct += (preds == labels).sum().item()
-           total += labels.size(0)
-           all_preds.extend(preds.cpu().numpy())
-           all_labels.extend(labels.cpu().numpy())
+def epoch_step(model, loader, criterion, optimizer=None, scaler=None, device='cpu'):
+    """
+    Run one epoch. If `optimizer` is None, runs in eval mode.
+    Supports AMP mixed precision through `scaler`.
+    Returns loss, accuracy, f1.
+    """
+    is_train = optimizer is not None
+    model.train() if is_train else model.eval()
 
-   acc = 100 * correct / total
-   f1 = f1_score(all_labels, all_preds, average='weighted')
-   return acc, f1
+    running_loss, all_preds, all_labels = 0.0, [], []
 
+    for imgs, labels in loader:
+        imgs   = imgs.to(device, non_blocking=True)
+        labels = labels.squeeze().long().to(device, non_blocking=True)
+
+        with torch.cuda.amp.autocast(enabled=scaler is not None):
+            outputs = model(imgs)
+            loss = criterion(outputs, labels)
+
+        if is_train:
+            optimizer.zero_grad(set_to_none=True)
+            if scaler:
+                scaler.scale(loss).backward()
+                scaler.step(optimizer)
+                scaler.update()
+            else:
+                loss.backward()
+                optimizer.step()
+
+        running_loss += loss.item()
+        preds = outputs.argmax(1)
+        all_preds.extend(preds.cpu().numpy())
+        all_labels.extend(labels.cpu().numpy())
+
+    loss_avg = running_loss / len(loader)
+    acc      = accuracy_score(all_labels, all_preds)
+    f1       = f1_score(all_labels, all_preds, average='weighted')
+    return loss_avg, acc, f1
