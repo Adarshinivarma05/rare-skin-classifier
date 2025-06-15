728853ded065268837e4656b33c8579f0a6a9348
Adarshinivarma05 <adarshinip2k25@gmail.com>
2025-06-15 08:27:07 +0000

    Update utils/visualizer.py from Colab

--- utils/visualizer.py
+++ utils/visualizer.py
@@ -1,17 +1,15 @@
 import matplotlib.pyplot as plt
 
-def plot_metrics(history: dict, filename='metrics.png'):
-    """Save a four‑panel training curve image."""
-    titles = ['Train Loss', 'Val Loss', 'Val Accuracy', 'Val F1']
-    keys   = ['train_loss', 'val_loss', 'val_acc', 'val_f1']
-
-    plt.figure(figsize=(14, 4))
-    for i, (k, t) in enumerate(zip(keys, titles)):
+def plot_metrics(history, filename='metrics.png'):
+    plt.figure(figsize=(12, 4))
+    for i, key in enumerate(['train_loss', 'val_loss', 'val_acc', 'val_f1']):
         plt.subplot(1, 4, i + 1)
-        plt.plot(history[k])
-        plt.title(t)
-        plt.grid(True)
+        plt.plot(history[key])
+        plt.title(key)
     plt.tight_layout()
     plt.savefig(filename)
     plt.close()
+
+
+
 
