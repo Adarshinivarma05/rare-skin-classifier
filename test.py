cf913a461ec3d22dc23fb5573dc484c260579741
Adarshinivarma05 <adarshinip2k25@gmail.com>
2025-06-15 09:03:19 +0000

    Update test.py from Colab

--- test.py
+++ test.py
@@ -4,15 +4,13 @@ from utils.data_loader import get_dataloaders
 from utils.train_utils import epoch_step
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = ProtoPNet().to(device)
+model = ProtoPNet().to(device).eval()
 model.load_state_dict(torch.load('best_model.pth'))
 
-_, _, test_loader = get_dataloaders(batch_size=32)
-criterion = torch.nn.CrossEntropyLoss()
-loss, acc, f1 = epoch_step(model, test_loader, criterion, None, None, device)
+_, _, test_loader, _ = get_dataloaders(batch_size=32)
+loss, acc, f1 = epoch_step(model, test_loader,
+                           torch.nn.CrossEntropyLoss(), device=device)
 print(f"Test Loss {loss:.3f} | Acc {acc*100:.2f}% | F1 {f1:.3f}")
-
-
 
 
 
