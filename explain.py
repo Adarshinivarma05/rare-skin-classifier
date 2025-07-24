4ec9162e66fd0898cf89584ea35158b2b7733d4a
Adarshinivarma05 <adarshinip2k25@gmail.com>
2025-07-24 20:51:02 +0530

    Update explain.py

--- explain.py
+++ explain.py
@@ -5,7 +5,8 @@ from utils.explain_utils import gradcam_vis
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = ProtoPNet().to(device)
-model.load_state_dict(torch.load("best_model.pth"))
+model.load_state_dict(torch.load("proto_resnet50.pth"))
+
 
 _, _, test_loader, _ = get_loaders(batch_size=1)
 img, _ = next(iter(test_loader))
